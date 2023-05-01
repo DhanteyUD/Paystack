bc3903d71258c4cfcd6d9094bdd1e239fb668754
Clinton Otse <clinton.otse@outsourceglobal.com>
2023-05-01 20:04:51 +0100

    Added Resources

--- paystack/src/components/paystack.jsx
+++ paystack/src/components/paystack.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './paystack.css';
+import DogIcon from '../assets/Dog-1-unscreen.gif';
 import PaystackPop from '@paystack/inline-js';
 
 function Paystack() {
   return (
     <main className='paystack_container'>
+      <span>
+        <img className='dog-icon' src={DogIcon} alt='Dog Icon' />
+      </span>
         <div className='header'>
-            <h1 className="text-focus-in">Paystack Payment Gateway</h1>
+            <h1 className="text-focus-in">Pet Fancy</h1>
+            <p className='slide-in-left'>Your favorite pet store</p>
         </div>
         <div className='card bounce-in-top'></div>
     </main>
