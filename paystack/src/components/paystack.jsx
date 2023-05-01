2cf2ce34face4f8bd04a7b1106db96ec914a90cc
Clinton Otse <clinton.otse@outsourceglobal.com>
2023-05-01 20:43:54 +0100

    Dog Icon 2 Added

--- paystack/src/components/paystack.jsx
+++ paystack/src/components/paystack.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './paystack.css';
-import DogIcon from '../assets/Dog-1-unscreen.gif';
+import DogIcon1 from '../assets/Dog-1-unscreen.gif';
+import DogIcon2 from '../assets/Dog-2.gif';
 import PaystackPop from '@paystack/inline-js';
 
 function Paystack() {
   return (
     <main className='paystack_container'>
       <span>
-        <img className='dog-icon' src={DogIcon} alt='Dog Icon' />
+        <img className='dog-icon-1 slide-out-left' src={DogIcon1} alt='Dog Icon 1' />
+      </span>
+      <span>
+        <img className='dog-icon-2 fade-in-fwd ' src={DogIcon2} alt='Dog Icon 2' />
       </span>
         <div className='header'>
             <h1 className="text-focus-in">Pet Fancy</h1>
