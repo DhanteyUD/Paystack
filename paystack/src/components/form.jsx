db22deb759a7ae8d95f85673bc17a168d72e7eed
Clinton Otse <clinton.otse@outsourceglobal.com>
2023-05-02 16:56:28 +0100

    Adds Form Functionalities

--- paystack/src/components/form.jsx
+++ paystack/src/components/form.jsx
@@ -1,6 +1,9 @@
 import './form.css';
 import React, { useState } from 'react'
+import DogIconForm from '../assets/Dog-4.gif';
 import { FormModel } from '../model/form.model.js';
+
+const DogIcon = 'https://res.cloudinary.com/dhantey/image/upload/v1683026512/Dog-8-unscreen_bb1s7y.gif'
 
 function Form() {
   const [form, setForm] = useState(FormModel);
@@ -10,79 +13,101 @@ function Form() {
     const { name, value } = e.target;
     setForm({...form, [name]: value});
   }
+  
+  const handleClearForm = (e) => {
+    e.preventDefault();
+    setForm(FormModel);
+  }
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
 
     setLoading(true);
-    alert('Loading...');
-  }
-
-  const handleClearForm = () => {
-    setForm(FormModel);
+    console.log(form);
+    setTimeout(() => {
+      setLoading(false);
+      setForm(FormModel);
+    }, 3000);
   }
 
   return (
     <>
       <div className='form-container'>
-        <form className='form-inner' onSubmit={handleSubmitForm}>
-          <div className="form-group">
-            <label htmlFor="firstName">First Name</label>
-            <input 
-              type="text" 
-              className="form-control" 
-              name="firstName" 
-              value={form.firstName}
-              onChange={handleChange}
-              placeholder="First Name" 
-            />
-
-            <label htmlFor="lastName">Last Name</label>
-            <input 
-              type="text" 
-              className="form-control" 
-              name="lastName" 
-              value={form.lastName}
-              onChange={handleChange}
-              placeholder="Last Name" 
-            />
-
-            <label htmlFor="email">Email</label>
-            <input 
-              type="email" 
-              className="form-control" 
-              name="email" 
-              value={form.email}
-              onChange={handleChange}
-              placeholder="example@gmail.com"  
-            />
-
-            <label htmlFor="firstName">Phone</label>
-            <input 
-              type="tel" 
-              className="form-control" 
-              name="phone" 
-              value={form.phone}
-              onChange={handleChange}
-              placeholder="+234" 
-            />
-
-            <label htmlFor="firstName">Amount</label>
-            <input 
-              type="number" 
-              className="form-control" 
-              name="amount" 
-              value={form.amount}
-              onChange={handleChange}
-              placeholder="₦"  
-              autoComplete="off"
-            />
+        {loading ? (
+          <div className='form-inner'>
+            <div className='form-inner-loading-div'>
+              <img className='loading-dog-icon' src={DogIcon} alt='Loading Dog Icon' />
+              <p className='loading-text'>loading, please wait...</p>
+            </div>
           </div>
-          <div className='button-group'>
-            <button type="clear" className="btn btn-secondary" onClick={handleClearForm}>Clear</button>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </div>
-        </form>
+        ) : (
+          <>
+            <span>
+              <img className='dog-icon-form' src={DogIconForm} alt='Dog Form Icon' />
+            </span>
+            <form className='form-inner'>
+              <h2 className='form-header'>Pet Fancy</h2>
+              <p className='form-sub-header'>Make <span className='donation'>donation</span> and save a Dog</p>
+
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  name="firstName" 
+                  value={form.firstName}
+                  onChange={handleChange}
+                  placeholder="First Name" 
+                />
+
+                <label htmlFor="lastName">Last Name</label>
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  name="lastName" 
+                  value={form.lastName}
+                  onChange={handleChange}
+                  placeholder="Last Name" 
+                />
+
+                <label htmlFor="email">Email</label>
+                <input 
+                  type="email" 
+                  className="form-control" 
+                  name="email" 
+                  value={form.email}
+                  onChange={handleChange}
+                  placeholder="example@gmail.com"  
+                />
+
+                <label htmlFor="firstName">Phone</label>
+                <input 
+                  type="tel" 
+                  className="form-control" 
+                  name="phone" 
+                  value={form.phone}
+                  onChange={handleChange}
+                  placeholder="+234" 
+                />
+
+                <label htmlFor="firstName">Amount</label>
+                <input 
+                  type="number" 
+                  className="form-control" 
+                  name="amount" 
+                  value={form.amount}
+                  onChange={handleChange}
+                  placeholder="₦"  
+                  autoComplete="off"
+                />
+              </div>
+              <div className='button-group'>
+                <button type='clear' className="btn btn-secondary" onClick={handleClearForm}>Clear</button>
+                <button type='submit' className="btn btn-primary" onClick={handleSubmitForm}>Submit</button>
+              </div>
+            </form>
+          </>
+        )}
       </div>
     </>
   )
